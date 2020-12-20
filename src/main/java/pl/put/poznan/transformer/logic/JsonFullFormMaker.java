ee0ce98be9de7357847932f3cc7fb35ce639a669
Hubert Cwajda <cwajdahubert@gmail.com>
2020-12-20 23:09:06 +0100

    Update JsonFullFormMaker.java

--- src/main/java/pl/put/poznan/transformer/logic/JsonFullFormMaker.java
+++ src/main/java/pl/put/poznan/transformer/logic/JsonFullFormMaker.java
@@ -5,13 +5,22 @@ import com.google.gson.GsonBuilder;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonParser;
 
+
+/**
+* Decorator class that formats json to human readeable form
+*/
 public class JsonFullFormMaker extends JsonDecorator {
+    
+    /**
+     * Constructor
+     * @param jsonObject internal decorator
+     */
     public JsonFullFormMaker(JSONObject jsonObject){ super(jsonObject); }
+    
      /**
      * this method takes string without white signs
      * @return formatted json, which is more human-readable
      */
-    
     @Override
     public String getJson(){
         Gson gson = new GsonBuilder().setPrettyPrinting().create();
