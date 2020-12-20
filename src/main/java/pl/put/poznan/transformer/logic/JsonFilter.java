3854fddc72e0fe68af810328c7b81b2ca00767af
Veltarrox <57348417+Veltarrox@users.noreply.github.com>
2020-12-20 22:00:31 +0100

    Update JsonFilter.java

--- src/main/java/pl/put/poznan/transformer/logic/JsonFilter.java
+++ src/main/java/pl/put/poznan/transformer/logic/JsonFilter.java
@@ -9,7 +9,12 @@ public class JsonFilter extends JsonDecorator {
         super(jsonObject);
         this.excludedKey = excludedKey;
     }
-
+    /**
+     * Returns Filtered JSON as String. It removes every instance of given code that is at most nested only once.
+     * It reads variable code, which starts from "exclude" and then element we want to filter is given
+     * for example"excludetitle" will remove every JSON with title as key.
+     * @return JSON String without specified keys
+     */
     @Override
     public String getJson() {
         var js = jsonObject.getJson();
