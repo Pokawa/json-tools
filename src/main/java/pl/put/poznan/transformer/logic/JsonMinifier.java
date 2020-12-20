1ef32971a1e05bf7991b582aedfff6a29b8f62e7
ciszekxd <44375349+ciszekxd@users.noreply.github.com>
2020-12-20 22:15:23 +0100

    Update JsonMinifier.java
    add comments

--- src/main/java/pl/put/poznan/transformer/logic/JsonMinifier.java
+++ src/main/java/pl/put/poznan/transformer/logic/JsonMinifier.java
@@ -4,7 +4,10 @@ public class JsonMinifier extends JsonDecorator {
     public JsonMinifier(JSONObject jsonObject) {
         super(jsonObject);
     }
-
+    /**
+     * this method takes format text
+     * @return string without white signs inside it
+     */
     @Override
     public String getJson() {
         String json = jsonObject.getJson();
