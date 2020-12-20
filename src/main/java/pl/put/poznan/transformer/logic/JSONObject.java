f7a913add3d0a90d2abc77e84c870684c18600c2
Veltarrox <57348417+Veltarrox@users.noreply.github.com>
2020-12-20 22:54:27 +0100

    Update JSONObject.java

--- src/main/java/pl/put/poznan/transformer/logic/JSONObject.java
+++ src/main/java/pl/put/poznan/transformer/logic/JSONObject.java
@@ -1,5 +1,7 @@
 package pl.put.poznan.transformer.logic;
-
+/**
+ * Class that stores JSON to be modified and by method getJson() allows to acces it at any given moment.
+ */
 public class JSONObject {
     private String json;
 
